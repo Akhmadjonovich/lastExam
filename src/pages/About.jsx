b707a0971de9f5d2be1f2a44f8e668ff77f43fc1
Akhmadjonovich <axrorbeknematov07@gmail.com>
2025-09-16 22:33:28 +0500

    about full

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,8 +8,8 @@ const About = () => {
           <h4 className='bg-[#FE9F6B] inline-block text-3xl max-2xl:text-2xl max-lg:text-xl font-bold p-2 rounded-xl'>Our mission</h4>
           <h2 className='text-7xl max-2xl:text-6xl max-xl:text-5xl max-lg:text-4xl font-bold my-10'>Help more people cook nourishing meals, 
           more often.</h2>
-          <p className='text-3xl text-[#395852] max-2xl:text-2xl max-xl:text-lg '>Healthy Recipe Finder was created to prove that healthy eating can be convenient, affordable, and genuinely delicious.</p>
-          <p className='text-3xl mt-10 max-lg:mt-5 text-[#395852] max-2xl:text-2xl max-xl:text-lg '>We showcase quick, whole-food dishes that anyone can master—no fancy equipment, no ultra-processed shortcuts—just honest ingredients and straightforward steps.</p>
+          <p className='text-3xl text-[#395852] max-2xl:text-2xl max-xl:text-xl '>Healthy Recipe Finder was created to prove that healthy eating can be convenient, affordable, and genuinely delicious.</p>
+          <p className='text-3xl mt-10 max-lg:mt-5 text-[#395852] max-2xl:text-2xl max-xl:text-xl '>We showcase quick, whole-food dishes that anyone can master—no fancy equipment, no ultra-processed shortcuts—just honest ingredients and straightforward steps.</p>
         </div>
         <img src="/images/image-about-our-mission-large.webp" className='w-[615px] max-2xl:w-[550px] max-xl:w-[500px] max-lg:w-[400px] max-md:w-full rounded-4xl' alt="" />
     </section>
@@ -82,7 +82,18 @@ const About = () => {
       </div>
     </section>
 
-
+    <section className='flex max-md:flex-col items-center justify-between gap-70 max-2xl:gap-50 max-xl:gap-25 max-md:gap-20 max-sm:gap-10'>
+      <div>
+        <h3 className='my-10 text-7xl max-2xl:text-6xl max-xl:text-5xl max-lg:text-4xl  font-bold'>Beyond the plate</h3>
+        <p className='text-3xl max-xl:text-2xl  text-[#395852]'>We believe food is a catalyst for community and well-being. By sharing approachable recipes, we hope to:</p>
+        <ol className='list-disc list-inside my-5 *:text-3xl *:max-xl:text-2xl  text-[#395852]'>
+          <li>Encourage family dinners and social cooking.</li>
+          <li>Reduce reliance on single-use packaging and delivery waste.</li>
+          <li>Spark curiosity about seasonal produce and local agriculture.</li>
+        </ol>
+      </div>
+      <img src="images/image-about-beyond-the-plate-large.webp" className='w-[744px] max-2xl:w-[680px] max-xl:w-[600px] max-lg:w-[450px] max-md:w-full rounded-3xl' alt="" />
+    </section>
     
 
     </div>
