6d771e3646857adc5d5b8e59197862e8b9bf0f79
Akhmadjonovich <axrorbeknematov07@gmail.com>
2025-09-18 10:10:15 +0500

    search commit

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,72 +1,197 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-const Recipes = () => {
 
-    const [data, setData] = useState([]); 
-    const [loading, setLoading] = useState(true); // Yuklanish holatini ko‘rsatish
-    let urlPoint = "https://recipes-api-3o3s.onrender.com/recipes" 
-    useEffect(() => {
-      
-      axios.get(urlPoint)
-        .then((response) => {
-          setData(response.data);   
-          setLoading(false);
-        })
-        .catch((error) => {
-          console.error("Xatolik:", error);
-          setLoading(false);
-        });
-    }, [urlPoint]); 
-  console.log(data);
-    if (loading) return <div className="flex items-center justify-center min-h-screen bg-gray-100">
+const Recipes = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [prepTime, setPrepTime] = useState("");
+  const [cookTime, setCookTime] = useState("");
+  const [openPrep, setOpenPrep] = useState(false);
+  const [openCook, setOpenCook] = useState(false);
+  const [search, setSearch] = useState("");
+
+  let urlPoint = "https://recipes-api-3o3s.onrender.com/recipes";
+
+  useEffect(() => {
+    axios
+      .get(urlPoint)
+      .then((response) => {
+        setData(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Xatolik:", error);
+        setLoading(false);
+      });
+  }, [urlPoint]);
+
+  
+  // search
+    
+  const filtered = data.filter(item => 
+    item.title.toLowerCase().includes(search.toLowerCase()) ||
+    item.overview.toLowerCase().includes(search.toLowerCase())
+  );
+
+  
+  const filteredData = data.filter((r) => {
+    let prepOk = prepTime ? r.prepMinutes <= Number(prepTime) : true;
+    let cookOk = cookTime ? r.cookMinutes <= Number(cookTime) : true;
+    return prepOk && cookOk;
+  });
+
+  
+  const Searched = search.trim() !== "";
+  const finalList = Searched ? filtered : filteredData;
+  
+  const times = [0, 5, 10, 15, 20];
+
+  if (loading)
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
         <div className="h-16 w-16 border-4 border-[#FE9F6B] border-t-transparent rounded-full animate-spin"></div>
-    </div>;
+      </div>
+    );
 
   return (
-    <div className='container mx-auto mt-20 '>
-        <h2 className='text-7xl max-2xl:text-6xl max-xl:text-5xl max-lg:text-4xl text-center my-10 font-bold'>Explore our simple, healthy recipes</h2>
-        <p className='text-3xl max-xl:text-2xl text-center text-[#395852] px-130 max-lg:px-90 max-md:px-50 max-sm:px-10 max-sm:text-xl'>Discover eight quick, whole-food dishes that fit real-life schedules and taste amazing. Use the search bar to find a recipe by name or ingredient, or simply scroll the list and let something delicious catch your eye.</p>
+    <div className="container mx-auto mt-20">
+      <h2 className="text-7xl max-2xl:text-6xl max-xl:text-5xl max-lg:text-4xl text-center my-10 font-bold">
+        Explore our simple, healthy recipes
+      </h2>
+      <p className="text-3xl max-xl:text-2xl text-center text-[#395852] px-130 max-lg:px-90 max-md:px-50 max-sm:px-10 max-sm:text-xl">
+      Discover eight quick, whole-food dishes that fit real-life schedules and taste amazing. Use the search bar to find a recipe by name or ingredient, or simply scroll the list and let something delicious catch your eye.
+      </p>
 
-        <section>
-            {/* FIlters */}
-        </section>
+      
+      <section className="flex gap-5 justify-between mx-10 my-10 relative">
+        <div className='flex gap-10'>
+        
+          <div 
+            className="relative inline-block"
+            onMouseEnter={() => setOpenPrep(true)}
+            onMouseLeave={() => setOpenPrep(false)} 
+          >
+            <button className="px-10 flex items-center py-1 h-18 text-2xl border border-transparent hover:border-black rounded-md bg-white shadow">
+              Max Prep Time <img src="/images/Down.svg"  alt="" />
+            </button>
+            
+            {openPrep && (
+              <div className="absolute left-0 mt-0.5 text-2xl  w-70 bg-white rounded-xl  shadow-xl z-10">
+                {times.map((time) => (
+                  <label
+                    key={time}
+                    className="flex items-center px-4 py-2 gap-2 cursor-pointer hover:bg-gray-100"
+                  >
+                    <input
+                      type="radio"
+                      value={time}
+                      checked={prepTime === String(time)}
+                      onChange={(e) => setPrepTime(e.target.value)}
+                    />
+                    {time} minutes
+                  </label>
+                ))}
+                <button
+                  onClick={() => setPrepTime("")}
+                  className="w-full text-left px-4 py-2 text-red-500 hover:bg-gray-100"
+                >
+                  Clear
+                </button>
+              </div>
+            )}
+          </div>
 
-        <section className='grid grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1 mx-10 max-sm:mx-5  gap-20 max-xl:gap-10 max-md:gap-20 '>
-            { 
-                data && data.map((r) => {
-                    return (
-                        <div key={r.id} className='relative bg-white  max-md:w-full rounded-2xl h-[103%] mt-20'>
-                            <img src={r.image.large} className='rounded-3xl p-3 max-sm:p-2 '  alt="" />
-                            <div className='p-5'>
-                                <h3 className='font-bold text-3xl'>{r.title}</h3>
-                                <p className='text-2xl max-lg:text-xl my-5 text-[#395852]'>{r.overview}</p>
 
-                                <div className='grid grid-cols-2 gap-5 text-2xl max-xl:text-xl max-lg:text-lg text-[#163A34]'>
-                                    <div className='flex items-center gap-5 max-lg:gap-3'>
-                                    <img src="/images/icon-servings.svg" alt="" />
-                                    <p>Servings: {r.servings}</p>
-                                </div>
-                                <div className='flex items-center gap-5'>
-                                    <img src="/images/icon-prep-time.svg" alt="" />
-                                    <p>Prep: {r.prepMinutes} mins</p>
-                                </div>
-                                <div className='flex items-center gap-5'>
-                                    <img src="/images/icon-cook-time.svg" alt="" />
-                                    <p>Cook: {r.cookMinutes} mins</p>
-                                </div>
-                                <button className='rounded-4xl absolute text-center bg-[#163A34]  py-5 text-white bottom-5 right-5 left-5 '>View Recipe</button>
-                        </div>
+        
+          <div 
+            className="relative inline-block "
+            onMouseEnter={() => setOpenCook(true)}
+            onMouseLeave={() => setOpenCook(false)}
+              
+          >
+            <button className="px-4 py-1 flex items-center h-18 border  border-transparent hover:border-black text-2xl rounded-md bg-white shadow">
+              Max Cook Time <img src="/images/Down.svg"  alt="" />
+            </button>
 
+            {openCook && (
+              <div className="absolute left-0 mt-1 w-70 text-2xl bg-white rounded-xl shadow-xl z-10">
+                {times.map((time) => (
+                  <label
+                    key={time}
+                    className="flex items-center px-4 py-2 gap-2 cursor-pointer hover:bg-gray-100"
+                  >
+                    <input
+                      type="radio"
+                      value={time}
+                      checked={cookTime === String(time)}
+                      onChange={(e) => setCookTime(e.target.value)}
+                    />
+                    {time} minutes
+                  </label>
+                ))}
+                <button
+                  onClick={() => setCookTime("")}
+                  className="w-full text-left px-4 py-2 text-red-500 hover:bg-gray-100"
+                >
+                  Clear
+                </button>
+              </div>
+            )}
+          </div>
+
+        
+        </div>
+
+        <div>
+            <form className='bg-white flex p-5 gap-2 w-md rounded-2xl '>
+                <img src="/images/Search.svg" alt="" />
+                <input 
+                 type="text"
+                 value={search}
+                 onChange={e => setSearch(e.target.value)} 
+                 className ='text-2xl w-full outline-none' placeholder='Search by name or ingredient…'/>
+
+            </form>
+        </div>
+      </section>
+
+      
+      <section className="grid grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1 mx-10 max-sm:mx-5 gap-20 max-xl:gap-10 max-md:gap-20">
+        {finalList.map((r) => (
+          <div
+            key={r.id}
+            className="relative bg-white max-md:w-full rounded-2xl h-[103%] mt-20"
+          >
+            <img src={r.image.large} className="rounded-3xl p-3 max-sm:p-2" alt="" />
+            <div className="p-5">
+              <h3 className="font-bold text-3xl">{r.title}</h3>
+              <p className="text-2xl max-lg:text-xl my-5 text-[#395852]">
+                {r.overview}
+              </p>
+
+              <div className="grid grid-cols-2 gap-5 text-2xl max-xl:text-xl max-lg:text-lg text-[#163A34]">
+                <div className="flex items-center gap-5 max-lg:gap-3">
+                  <img src="/images/icon-servings.svg" alt="" />
+                  <p>Servings: {r.servings}</p>
                 </div>
-
+                <div className="flex items-center gap-5">
+                  <img src="/images/icon-prep-time.svg" alt="" />
+                  <p>Prep: {r.prepMinutes} mins</p>
+                </div>
+                <div className="flex items-center gap-5">
+                  <img src="/images/icon-cook-time.svg" alt="" />
+                  <p>Cook: {r.cookMinutes} mins</p>
+                </div>
+                <button className="rounded-4xl absolute text-center bg-[#163A34] py-5 text-white bottom-5 right-5 left-5">
+                  View Recipe
+                </button>
+              </div>
             </div>
-                    )
-                })
-            }
-        </section>
+          </div>
+        ))}
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Recipes
+export default Recipes;
